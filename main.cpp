2e164ed5bf47ed98a073b727af38373f287dd7bc
iskakoff <sir.iskakoff@gmail.com>
2022-06-15 15:30:44 -0400

    Dependency

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <vertex.h>
-//#include <mpi.h>
+#include <mpi.h>
 
 int main(int argc, char ** argv) {
+  MPI_Init(&argc, &argv);
   vertex v;
-//  MPI_Init(&argc, &argv);
 
 
-//  MPI_Finalize();
+  MPI_Finalize();
 }
